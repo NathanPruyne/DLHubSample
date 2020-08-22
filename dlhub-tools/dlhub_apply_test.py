12bdd40e826e82d1c86f294a8bbb768f2da29c2c
NathanPruyne <nathan.pruyne@gmail.com>
2020-08-22 04:39:33 -0500

    Remake of dlhub_apply_test

--- dlhub-tools/dlhub_apply_test.py
+++ dlhub-tools/dlhub_apply_test.py
@@ -1,23 +1,40 @@
 from dlhub_apply import apply
 
+from dlhub_sdk.client import DLHubClient
+import glob
+import os
+import argparse
+import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
-import numpy as np
 
-def test_apply():
+def apply_model(model_file, norms_file, path, outpath):
+    data_path = path
+    files = glob.glob(os.path.join(data_path, '*.png'))
 
-    data_path = './data/tomography/test/images/c70_ObjT1_z171.png'
     imgs = []
-    with open(data_path, 'rb') as fp:
-        imgs.append(np.array(Image.open(fp).convert('RGB')))
-    out = apply(imgs, model_file='PFNetwork.pth', norms_file='PFnormsnew.txt')
-    print(out[0].shape)
+    for img_path in files:
+        with open(img_path, 'rb') as fp:
+            imgs.append(np.array(Image.open(fp).convert('RGB')))
 
-    for seg in out:
-        plt.imsave('dlhub_result_new_norms.png', seg, cmap='gray')    
+    outs = apply(imgs, model_file=model_file, norms_file=norms_file)
 
-    return out
+    num = 0
+
+    for seg in outs:
+
+        print("Saving " + os.path.split(files[num])[1])
+        plt.imsave(outpath + os.path.split(files[num])[1], seg, cmap='gray')
+        num += 1
 
 if __name__ == "__main__":
-    out = test_apply()
-    print(out)
+    parser = argparse.ArgumentParser(description="Apply images to a model via DLHub")
+
+    parser.add_argument('model_file', help='.pth file containing model information')
+    parser.add_argument('norms_file', help='.txt file containing norms information')
+    parser.add_argument("filepath", help="Path to a directory with files to process")
+    parser.add_argument("outpath", help="Path to a directory to store result files in")
+
+    args = parser.parse_args()
+
+    apply_model(args.model_file, args.norms_file, args.filepath, args.outpath)
